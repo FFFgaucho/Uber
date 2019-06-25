f734b0acf1faea2b14257e7d1272fffa40e499ae
sapiens <fel2009freitas@gmail.com>
2019-06-25 18:43:39 -0300

    25-06 filtro pronto com tmdb

--- src/app/feed/feed.page.ts
+++ src/app/feed/feed.page.ts
@@ -11,55 +11,37 @@ import { MoooovieService } from '../moooovie.service';
   ]
 })
 export class FeedPage implements OnInit {
-  public nome_usuario:string = "Felipe Freitas";
+ // public nome_usuario:string = "Felipe Freitas";
   
   public lista_filmes = new Array<any>();
-  public lista_temp = new Array<any>();
-  public i: number = 0;
-  
   public searchTerm: string = "";
-  public items: any;
-  
-  public obj: any;
-  public result: any;
+  public resp: any;
 
   constructor(
     private router: Router,
     private testProvider: MoooovieService) { }
 
   ngOnInit() { //código do que acontece na página quando chama 
-    //this.setFilteredItems();
-    //this.getFilmes();
-    this.filtro();
+    this.getMovies();
   }
 
-  setFilteredItems() {
-    /*console.log(this.searchTerm);
-    if(this.searchTerm==""){
-      console.log("NUloooo");
-      this.items = null;
-    }
-    else*/
-      this.items = this.testProvider.filterItems(this.searchTerm);
-      console.log("Valorrr");
-
-  }/*
-  //Funçãoes relativas à página
-  feedSelect(){
-    alert("ROdou");
+  getMovies(){
+    this.resp = this.testProvider.getApi();
+    this.resp.subscribe(
+      data=>{
+        const response = (data as any);
+        const objeto_retorno = JSON.parse(response._body);
+        this.lista_filmes = objeto_retorno.results;
+        //cópia para provider
+        this.testProvider.setListaFilmes(this.lista_filmes);
+      },
+      error=>{
+        console.log(error);
+      }
+    ); 
   }
-  //getFilmes(){ //inicializa o lista filme
-  //  ); 
-  //}*/
+
   filtro(){
-    /*if(this.searchTerm==""){
-      this.lista_filmes=this.lista_temp;
-      this.i=this.i+1;
-      console.log(this.i);
-    }    
-    else*/
-    this.lista_filmes=this.testProvider.filterFilmes(this.searchTerm);  
-      
+    this.lista_filmes=this.testProvider.filterFilmes(this.searchTerm);       
   }
-  
 }
--- src/app/moooovie.service.ts
+++ src/app/moooovie.service.ts
@@ -5,53 +5,22 @@ import { Http} from '@angular/http';
   providedIn: 'root'
 })
 export class MoooovieService {
-  public resp: any;
+
   public items: any = []; //para teste
   public lista_filmes = new Array<any>();
 
 
   constructor( public http:Http) { 
-    this.items = [
-      { title: "one" },
-      { title: "two" },
-      { title: "three" },
-      { title: "four" },
-      { title: "five" },
-      { title: "six" }
-    ];
-    this.getLatestMovies();
-
-  }
-  getLatestMovies(){
-    this.resp = this.getApi();
-    this.resp.subscribe(
-      data=>{
-        const response = (data as any);
-        const objeto_retorno = JSON.parse(response._body);
-        this.lista_filmes = objeto_retorno.results;
-        //this.lista_temp=this.lista_filmes; //cópia 
-        console.log(this.lista_filmes);
-        console.log("Mudou");
-        //console.log(this.lista_temp);
-        
-      },
-      error=>{
-        console.log(error);
-      }
-    ); 
   }
   getApi(){
     return this.http.get("https://api.themoviedb.org/3/movie/popular?api_key=8534a98ab60ad65e84061b04220ce97b");
   }
+  setListaFilmes(obj: any){
+    this.lista_filmes = obj;
+  }
   filterFilmes(searchTerm) { //para teste
     return this.lista_filmes.filter(filme => {
       return filme.original_title.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1;
-    });
-  }
-  
-  filterItems(searchTerm) { //para teste
-    return this.items.filter(item => {
-      return item.title.toLowerCase().indexOf(searchTerm.toLowerCase()) > -1;
     });
   }
 }
