96e7d1b582f076b8993445d5a5bd631d2e788670
sapiens <fel2009freitas@gmail.com>
2019-07-10 13:12:12 -0300

    filtro pronto com tmdb

--- src/app/moooovie.service.ts
+++ src/app/moooovie.service.ts
@@ -1,3 +1,4 @@
+
 import { Injectable } from '@angular/core';
 import { Http} from '@angular/http';
 
@@ -8,32 +9,13 @@ export class MoooovieService {
 
   public items: any = []; //para teste
   public lista_filmes = new Array<any>();
-  resp: any;
 
 
   constructor( public http:Http) { 
   }
   getApi(){
-    this.resp = this.http.get("https://api.themoviedb.org/3/movie/popular?api_key=8534a98ab60ad65e84061b04220ce97b");
-    this.resp.subscribe(
-      data=>{
-        const response = (data as any);
-        const objeto_retorno = JSON.parse(response._body);
-        this.lista_filmes = objeto_retorno.results;
-        //cópia para provider
-        //this.testProvider.setListaFilmes(this.lista_filmes);
-        console.log("provider");
-        console.log(this.lista_filmes);
-      },
-      error=>{
-        console.log(error);
-      }
-    );
-
-    return this.lista_filmes; 
-
+    return this.http.get("https://api.themoviedb.org/3/movie/popular?api_key=8534a98ab60ad65e84061b04220ce97b");
   }
-  
   setListaFilmes(obj: any){
     this.lista_filmes = obj;
   }
