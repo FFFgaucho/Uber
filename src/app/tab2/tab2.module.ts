e8bfa7dc933f8668da8486e8cf017c2ca29b9f7a
FFFgaucho <fel2009freitas@gmail.com>
2019-08-23 18:20:30 -0300

    Cadastrar serviço quase

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -4,13 +4,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
+import {NgbDropdownModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab2Page }])
+    RouterModule.forChild([
+      {
+        path: '', 
+        component: Tab2Page 
+      }
+    ]),
+    NgbDropdownModule
   ],
   declarations: [Tab2Page]
 })
